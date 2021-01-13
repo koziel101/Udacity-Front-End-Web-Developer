d312d453d28fa2b7e2ae4acb6117bffcd6c42e9f
HiagoKoziel <hiagokoziel100@gmail.com>
2021-01-13 18:33:22 +0100

    When Generate button is clicked, Weather API is successfully called with the post code provided by the user

--- Project 3 Weather Journal/website/app.js
+++ Project 3 Weather Journal/website/app.js
@@ -1,18 +1,15 @@
-/* Global Variables */
-
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
-
 
 // Personal API Key for OpenWeatherMap API
 // To call OpenWeather API by Zip Code:
 // api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={API key}
 const baseUrl = "https://api.openweathermap.org/data/2.5/weather?zip=";
 const apiKey = "&appid=eb34694edecea0a4472b2e26672b6d04";
-const imperialMetric = "&units=imperial";
+const celsiusMetric = "&units=metric";
 // Zip code to test: 10001
-// Working example url: https://api.openweathermap.org/data/2.5/weather?zip=10001&appid=eb34694edecea0a4472b2e26672b6d04&units=imperial
+// Working example url: https://api.openweathermap.org/data/2.5/weather?zip=10001&appid=eb34694edecea0a4472b2e26672b6d04&units=metric
 
 const postData = async(url = '', data = {}) => {
     console.log(data);
@@ -35,15 +32,38 @@ const postData = async(url = '', data = {}) => {
     }
 }
 
-document.getElementById('generate').addEventListener('click', performAction);
+// Listening for when the "Generate" button is clicked
+document.getElementById('generate').addEventListener('click', buttonClicked);
 
-function performAction(e) {
-    console.log("Test");
+function buttonClicked(e) {
+    const zipCode = document.getElementById('zip').value;
+    if (zipCode.length != 5) {
+        window.alert("Zip code must have 5 numbers! It currently has " + zipCode.length + " characters.");
+    } else {
+        if (isNumeric(zipCode)) {
+            getWeather(baseUrl, zipCode, apiKey, celsiusMetric);
+        } else {
+            window.alert("Zip code must be only numbers!");
+        }
+    }
 }
 
-postData('/addWeather', { movie: "Matrix", weather: 24 });
+// Check if the zip code typed is numeric
+function isNumeric(value) {
+    return /^-?\d+$/.test(value);
+}
 
-
+const getWeather = async(baseURL, zipCode, key, celsiusMetric) => {
+    const res = await fetch(baseURL + zipCode + key + celsiusMetric);
+    try {
+        const data = await res.json();
+        console.log(data)
+        return data;
+    } catch (error) {
+        console.log("Error: ", error);
+        // appropriately handle the error
+    }
+}
 
 // Event listener to add function to existing HTML DOM element
 
@@ -52,6 +72,5 @@ postData('/addWeather', { movie: "Matrix", weather: 24 });
 /* Function to GET Web API Data*/
 
 /* Function to POST data */
-
 
 /* Function to GET Project Data */
