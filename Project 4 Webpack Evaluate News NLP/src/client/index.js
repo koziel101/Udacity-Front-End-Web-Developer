969d91e577f19fcd3a545f27e0a3e4baace0755f
HiagoKoziel <hiagokoziel100@gmail.com>
2021-01-29 15:34:36 +0100

    Adding footer and images to the project

--- Project 4 Webpack Evaluate News NLP/src/client/index.js
+++ Project 4 Webpack Evaluate News NLP/src/client/index.js
@@ -6,6 +6,19 @@ import './styles/footer.scss'
 import './styles/form.scss'
 import './styles/header.scss'
 
+// Importing image files for the footer
+import gitHubIconIcon from "./images/github.jpg";
+import linkedInIconIcon from "./images/logo-linkedin.jpg";
+import faceBookIconIcon from "./images/logo-fb.png";
+
+
+var gitHub = document.querySelector("#github__logo");
+gitHub.src = gitHubIconIcon;
+var linkedIn = document.querySelector("#linkedin__logo");
+linkedIn.src = linkedInIconIcon;
+var faceBook = document.querySelector("#fb__logo");
+faceBook.src = faceBookIconIcon;
+
 export {
     checkForName,
     handleSubmit
