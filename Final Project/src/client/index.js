1938a9e89b26cc632f14928dc69da612f28ba7dd
HiagoKoziel <hiagokoziel100@gmail.com>
2021-02-03 18:30:22 +0100

    Trip information is now being displayed in the UI. for the user. However, only for trips with departure up to 5 days.

--- Final Project/src/client/index.js
+++ Final Project/src/client/index.js
@@ -2,8 +2,10 @@ import './styles/base.scss'
 import './styles/navbar.scss'
 import { buttonClicked } from './js/mainClient'
 
+/*
 import cityView from "./images/example.jpg";
 var cityElement = document.querySelector("#city-img");
 cityElement.src = cityView;
+*/
 
 export { buttonClicked }
--- Final Project/src/client/js/mainClient.js
+++ Final Project/src/client/js/mainClient.js
@@ -3,7 +3,7 @@ import { calculateDateDifference } from "./dayDifference"
 let appData = {};
 let departureDate = {};
 let returnDate = {};
-let dayDifference = {};
+let dayDifference = [];
 let hasTrip = false;
 
 document.getElementById('btn-new-trip').addEventListener('click', buttonClicked);
@@ -12,7 +12,8 @@ function buttonClicked(e) {
     appData = document.getElementById('city').value;
     departureDate = document.getElementById('departure-date').value;
     returnDate = document.getElementById('return-date').value;
-    dayDifference = calculateDateDifference(departureDate);
+    //dayDifference = calculateDateDifference(departureDate);
+    dayDifference.push(calculateDateDifference(departureDate));
 
     if (validateUserEntry(appData)) {
         // Sending data to server, so geonames API will get lat and lng
@@ -21,6 +22,7 @@ function buttonClicked(e) {
             .then(() => fetch("http://localhost:3000/validateCity"))
             .then(res => res.json())
             .then(data => {
+                console.log("New data was received from geonames server.")
                 if (data.totalResultsCount === 0) {
                     window.alert("City provided is not valid!");
                 } else if (!validateUserEntry(departureDate)) {
@@ -30,21 +32,17 @@ function buttonClicked(e) {
                     postData('http://localhost:3000/addWeather', {
                         lat: data.geonames[0].lat,
                         lng: data.geonames[0].lng,
-                        dayDifference: dayDifference
+                        dayDifference: dayDifference[dayDifference.length - 1]
                     })
                         // Retrieving data provided by Weatherbit API
                         .then(() => fetch("http://localhost:3000/checkWeather"))
                         .then(res => res.json())
                         .then(data => {
-                            console.log("New data was received from weatherbit server")
-                            console.log(data.data[0].weather.description)
-                            console.log("Full info: ")
-                            console.log(data)
+                            console.log("New data was received from weatherbit server.")
                             updateUI(data);
                         })
                 }
             })
-
     } else {
         window.alert("Text cannot be blank!");
     }
@@ -69,16 +67,31 @@ const postData = async (url = '', data = {}) => {
     }
 }
 
-export function updateUI() {
+export function updateUI(data) {
     if (!hasTrip) {
         document.getElementById("no-trip-planned").remove();
         hasTrip = true;
     }
-    let mytrip = document.createElement('span');
-    mytrip.innerText = "My trip to: ";
 
-    let tripContent = document.getElementById("trip-content");
-    tripContent.insertAdjacentElement('afterbegin', mytrip)
+    // Days away until the trip:
+    let myTripTo = document.getElementById("my-trip-to");
+    myTripTo.innerText = "Details about my trip to " + appData;
+
+    // Days away until the trip:
+    let daysAway = document.getElementById("days-away");
+    if (dayDifference[dayDifference.length - 1] === 0) {
+        daysAway.innerText = "The trip is today!";
+    } else if (dayDifference[dayDifference.length - 1] === 1) {
+        daysAway.innerText = dayDifference[dayDifference.length - 1] + " day until the trip!";
+    } else if (dayDifference[dayDifference.length - 1] < 0) {
+        daysAway.innerText = "The departure already happened " + (dayDifference[dayDifference.length - 1] * -1) + " days ago";
+    } else {
+        daysAway.innerText = dayDifference[dayDifference.length - 1] + " days until the trip!";
+    }
+
+    // How is the weather:
+    let howIsWeather = document.getElementById("hows-weather");
+    howIsWeather.innerText = "How's the weather: " + data.data[0].weather.description;
 }
 
 export { buttonClicked }
