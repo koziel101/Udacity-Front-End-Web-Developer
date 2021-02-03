86612ac579dfc6ee63ee125a287e90aef50f31d6
HiagoKoziel <hiagokoziel100@gmail.com>
2021-02-03 17:47:09 +0100

    Adding initial idea for UI in mobile device

--- Final Project/src/client/index.js
+++ Final Project/src/client/index.js
@@ -2,4 +2,8 @@ import './styles/base.scss'
 import './styles/navbar.scss'
 import { buttonClicked } from './js/mainClient'
 
+import cityView from "./images/example.jpg";
+var cityElement = document.querySelector("#city-img");
+cityElement.src = cityView;
+
 export { buttonClicked }
